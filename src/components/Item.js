05cffb1f42bc754b2f2db0391d7c3be052af6c1a
Bessmack <jenwied4lif@gmail.com>
2025-07-14 15:48:21 +0300

    Complete
    React-hooks-state-and-events  Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(true)
+
+  function handleInCart(){
+    setIsInCart(() => !isInCart)
+  }
+  console.log(isInCart)
+
   return (
-    <li className="">
+    <li className={isInCart ? "" : "in-cart"}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isInCart ? "add" : "remove"} onClick={handleInCart} >{isInCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
